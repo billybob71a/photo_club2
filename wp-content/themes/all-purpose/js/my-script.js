701228dbaafebac245aed52cfa39b668d24fb91c
root <root@www.visorsourcing.com>
2018-10-30 00:21:07 -0500

    Changing verbiage on confirm button in jQuery and removing debug log from git

--- wp-content/themes/all-purpose/js/my-script.js
+++ wp-content/themes/all-purpose/js/my-script.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($){
 	var body = $('body');
 	$('input').click(function(){
 		if ($('input').val() == 'Delete') {
-			var isGood = confirm('Did you press a button');
+			var isGood = confirm('Are you sure you want delete this photo?');
 			var post_number = $(this).attr('name');
 			if (isGood) {
 				$.ajax({ url: 'https://www.visorsourcing.com/wp-content/themes/all-purpose/delete_post.php',
