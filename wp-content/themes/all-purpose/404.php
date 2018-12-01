e9bde4c57a0c56211a07a1d41f046f8d30353208
root <root@www.visorsourcing.com>
2018-12-01 00:41:07 -0600

    updating 404 errors to not allow users to browse the posts updating theme-functions.php to use the original email new registration updating theme-functions.php to not generate random password updating theme-functions.php to remove password hint

--- wp-content/themes/all-purpose/404.php
+++ wp-content/themes/all-purpose/404.php
@@ -14,41 +14,41 @@ get_header(); ?>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'all-purpose' ); ?></p>
+					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links above or a search?', 'all-purpose' ); ?></p>
 
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
+					//<?php
+					//	get_search_form();
+					//
+					//	the_widget( 'WP_Widget_Recent_Posts' );
 
 						// Only show the widget if site has multiple categories.
-						if ( all_purpose_categorized_blog() ) :
-					?>
+					//	if ( all_purpose_categorized_blog() ) :
+					//?>
 
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'all-purpose' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
+					//<div class="widget widget_categories">
+					//	<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'all-purpose' ); ?></h2>
+					//	<ul>
+					//	<?php
+					//		wp_list_categories( array(
+					//			'orderby'    => 'count',
+					//			'order'      => 'DESC',
+					//			'show_count' => 1,
+					//			'title_li'   => '',
+					//			'number'     => 10,
+					//		) );
+					//	?>
+					//	</ul>
+					// </div><!-- .widget -->
 
-					<?php
-						endif;
+					//<?php
+					//	endif;
 
 						/* translators: %1$s: smiley */
-						$ap_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'all-purpose' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$ap_archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+					//	$ap_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'all-purpose' ), convert_smilies( ':)' ) ) . '</p>';
+					//	the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$ap_archive_content" );
+					//
+					//	the_widget( 'WP_Widget_Tag_Cloud' );
+					//?>
 
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
