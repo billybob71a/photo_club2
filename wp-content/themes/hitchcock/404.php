89741b3031ff057b7f10c884baff2035deb5c80f
Peter Yung <yungpet@gmail.com>
2020-03-23 19:15:26 -0600

    updating files to include child-theme for hitchcock

--- wp-content/themes/hitchcock/404.php
+++ wp-content/themes/hitchcock/404.php
@@ -18,9 +18,8 @@
 		        	            
 		            <p><?php _e( "It seems like you have tried to open a page that doesn't exist. It could have been deleted, moved, or it never existed at all. You are welcome to search for what you are looking for with the form below.", 'hitchcock' ); ?></p>
 		            
-		            <?php get_search_form(); ?>
-		            
-		        </div><!-- .post-content -->
+		           <!--<?php get_search_form(); ?> -->
+		        </div><!-- .post-content -->
 		        
 			</div><!-- .post-inner -->
         
