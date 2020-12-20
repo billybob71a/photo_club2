f65e3880a22dca20f94e661a358504e23988f0af
Peter Yung <yungpet@gmail.com>
2020-12-20 15:16:39 -0700

    mobile script for down caret collapsible menu adding version number can force refresh so that users will have up-to-date version number instead of stale cache javascript

--- wp-content/themes/hitchcock-child/functions.php
+++ wp-content/themes/hitchcock-child/functions.php
@@ -234,12 +234,12 @@ add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 /* Peter Y custom functions
 custom function to make home page transparent */
 if ( wp_is_mobile() ) { function add_petery_script() {
-    wp_register_script('petery_script_mobile', get_template_directory_uri() . '/js/petery_script_mobile.js', array( 'jquery' ),'',true);
+    wp_register_script('petery_script_mobile', get_template_directory_uri() . '/js/petery_script_mobile.js', array( 'jquery' ),'1.1',true);
     wp_enqueue_script('petery_script_mobile');
 }
 }
 else { function add_petery_script() {
-    wp_register_script('petery_script', get_template_directory_uri() . '/js/petery_script.js', array( 'jquery' ),'',true);
+    wp_register_script('petery_script', get_template_directory_uri() . '/js/petery_script.js', array( 'jquery' ),'1.1',true);
     wp_enqueue_script('petery_script');
 }
 }
