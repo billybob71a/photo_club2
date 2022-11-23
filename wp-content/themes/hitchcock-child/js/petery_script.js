a243fe84855f337165073937090f9a5d0fd45009
Peter Yung <yungpet@gmail.com>
2022-11-23 15:04:04 -0700

    changing the reference ip address to the dev server

--- wp-content/themes/hitchcock-child/js/petery_script.js
+++ wp-content/themes/hitchcock-child/js/petery_script.js
@@ -7,7 +7,7 @@ $(".post-container").on('click','#delete_button', function(){
     var isGood = confirm('Are you sure you want delete this photo?');
     var post_number = $(this).attr('name');
     if (isGood) {
-        $.ajax({ url: 'https://www.cpas-yyc.com/wp-content/themes/hitchcock-child/delete_post.php',
+        $.ajax({ url: 'http://63.142.254.125/wp-content/themes/hitchcock-child/delete_post.php',
             data: {delete_var: post_number},
             type: 'post',
             success: function(output) {
