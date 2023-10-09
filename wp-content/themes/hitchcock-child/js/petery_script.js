6cb01354399ef84b73bb274895ceb95a9e8dcdf5
billybob71a <yungpet@gmail.com>
2023-10-09 13:44:24 -0600

    correcting regex and using variable for http or https

--- wp-content/themes/hitchcock-child/js/petery_script.js
+++ wp-content/themes/hitchcock-child/js/petery_script.js
@@ -5,17 +5,18 @@ jQuery('a[href="#pll_switcher"]').click(function(){$(this).next().hide().slideDo
 jQuery('li.item_welcome').css('color','green');
 const url_val = window.location.href;
 //regex that uses groups match to find the server name of the URL
-const regex = new RegExp(`((\/\/)(.*?)(\/))`, "g");
+const regex = new RegExp(`((^[h].*?[:])(\/\/)(.*?)(\/))`, "g");
 var match = regex.exec(url_val);
 if (match) {
-    var serverName = match[3];
+    var serverName = match[4];
+    var http_https = match[3];
 }
 
 $(".post-container").on('click','#delete_button', function(){
     var isGood = confirm('Are you sure you want delete this photo?');
     var post_number = $(this).attr('name');
     if (isGood) {
-        $.ajax({ url: 'http://'+ serverName +'/wp-content/themes/hitchcock-child/delete_post.php',
+        $.ajax({ url: http_https + '//' + serverName +'/wp-content/themes/hitchcock-child/delete_post.php',
             data: {delete_var: post_number},
             type: 'post',
             success: function(output) {
