2a9e13c6fa6a9f7679a1387d4d7b2c97b58b8a81
billybob71a <yungpet@gmail.com>
2025-12-14 01:00:09 -0700

    taking into account apostrophe in file name

--- cpasadmin.php
+++ cpasadmin.php
@@ -35,6 +35,8 @@ function display_photos_all($keys_of_division_unique, $sub_folder_current) {
     $list_file = $_SERVER['DOCUMENT_ROOT'] . '/wp-content/uploads/' . $sub_folder_current . '/photo_random/LEVEL_0/LEVEL_0_list.txt';
     $list_file_dir = $_SERVER['DOCUMENT_ROOT'] . '/wp-content/uploads/' . $sub_folder_current . '/photo_random/LEVEL_0/';
     $list_file_contents = file($list_file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    $search = "'";    // Find the single quote
+    $replace = "\\'"; // Replace it with a backslash followed by a single quote
     if ($list_file_contents  !== false ) {
         foreach ($list_file_contents as $line) {
             preg_match($pattern, $line, $matches );
@@ -95,6 +97,7 @@ function display_photos_all($keys_of_division_unique, $sub_folder_current) {
                     }
 
                     $trimFileName = trim($line_split[1]);
+                    $trimFileName = str_replace($search, $replace, $trimFileName);
                     $fileMatch = $list_file_contents_array[$trimFileName];
                     // $fileAndLocation = $list_file_dir . $line_split[1];
                     $fileSizeVar = filesize($list_file_dir . $fileMatch);
