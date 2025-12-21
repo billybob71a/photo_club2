18ebd25eb085c1c1a958762176a9b236fbca91aa
billybob71a <yungpet@gmail.com>
2025-12-21 00:22:27 -0700

    correcting matches var to check for empty array

--- cpasadmin.php
+++ cpasadmin.php
@@ -42,7 +42,7 @@ function display_photos_all($keys_of_division_unique, $sub_folder_current) {
         foreach ($list_file_contents as $line) {
             preg_match($pattern, $line, $matches );
             $matches[1] = str_replace($search, $replace, $matches[1]);
-            if ($matches[1] = []) {
+            if ($matches = []) {
                 echo(" match not found matches[1] = " . var_dump($matches[1]) . " and line is " . $line);
             }
             $list_file_contents_array[$matches[1]] = $matches[3];
